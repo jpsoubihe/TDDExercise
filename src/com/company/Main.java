ac13a6739fc116b8f60cad9b1d31a2dc543cd0e8
Joao Paulo Soubihe <j151106@students.ic.unicamp.br>
2020-12-23 01:15:33 -0300

    [red] First commit for our TDD exercise
    Created a class and the first test for it

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("test");
+        Item i = new Item("testItem");
+	    System.out.println(i.toString());
+
+
+    }
+}
