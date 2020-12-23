3c433c5bbb9aed3a752a7cf404848c425c73a7af
Joao Paulo Soubihe <j151106@students.ic.unicamp.br>
2020-12-23 01:20:01 -0300

    First commit of our TDDExercise
    Class FileList.java created and first test implemented

--- src/com/company/FileList.java
+++ src/com/company/FileList.java
@@ -1,4 +1,4 @@
 package com.company;
 
-public class Files {
+public class FileList {
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("test");
-        Item i = new Item("testItem");
-	    System.out.println(i.toString());
-
-
     }
 }
