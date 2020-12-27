d2154130a9825186ec0fd804445ac0c23dcaec29
Joao Paulo Soubihe <j151106@students.ic.unicamp.br>
2020-12-27 15:53:59 -0300

    [red] Tests fo list limit and consequently effects

--- src/com/company/FileListTest.java
+++ src/com/company/FileListTest.java
@@ -2,12 +2,17 @@ package com.company;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
+
 import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FileListTest {
 
+    private static final String FILE_A= "FileA";
+    private static final String FILE_B = "FileB";
+    private static final String FILE_C = "FileC";
 
     /**
      * On first application execution it should have an empty list of recent files.
@@ -21,25 +26,57 @@ class FileListTest {
     @Test
     public void openFileShouldAddToRecentFileList() {
         FileList fileList = new FileList();
-        fileList.openFile("FileA");
-        assertEquals(fileList.getRecent(), asList("FileA"));
+        fileList.openFile(FILE_A);
+        assertEquals(fileList.getRecent(), Collections.singletonList(FILE_A));
     }
 
     @Test
     public void openMultipleFilesShouldStoreMostRecentInTop() {
         FileList fileList = new FileList();
-        fileList.openFile("FileA");
-        fileList.openFile("FileB");
-        fileList.openFile("FileC");
-        assertEquals(fileList.getRecent(), asList("FileC", "FileB", "FileA"));
+        fileList.openFile(FILE_A);
+        fileList.openFile(FILE_B);
+        fileList.openFile(FILE_C);
+        assertEquals(asList(FILE_C, FILE_B, FILE_A), fileList.getRecent());
     }
 
     @Test
     public void shouldNotContainDuplicates(){
         FileList fileList = new FileList();
-        fileList.openFile("FileA");
-        fileList.openFile("FileB");
-        fileList.openFile("FileA");
-        assertEquals(fileList.getRecent(), asList("FileA", "FileB"));
+        fileList.openFile(FILE_A);
+        fileList.openFile(FILE_B);
+        fileList.openFile(FILE_A);
+        assertEquals(asList(FILE_A, FILE_B), fileList.getRecent());
+    }
+
+    @Test
+    public void shouldNotPassListLimit() {
+        int limit = 2;
+        FileList fileList = new FileList(2);
+        assertEquals(2, fileList.limit);
+        fileList.openFile(FILE_A);
+        fileList.openFile(FILE_B);
+        fileList.openFile(FILE_C);
+        assertEquals(limit, fileList.getRecent().size());
+    }
+
+    @Test
+    public void shouldRemoveCaseFullList() {
+        int limit = 2;
+        FileList fileList = new FileList(2);
+        fileList.openFile(FILE_A);
+        fileList.openFile(FILE_B);
+        fileList.openFile(FILE_C);
+        assertEquals(asList(FILE_C,FILE_B), fileList.getRecent());
+    }
+
+    @Test
+    public void shouldRemoveOldestInCaseFullList() {
+        int limit = 2;
+        FileList fileList = new FileList(2);
+        fileList.openFile(FILE_A);
+        fileList.openFile(FILE_B);
+        fileList.openFile(FILE_C);
+        fileList.openFile(FILE_A);
+        assertEquals(asList(FILE_A,FILE_C), fileList.getRecent());
     }
 }
